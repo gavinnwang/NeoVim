578c220d009a95dedc46a8d988ed06e2d10490aa
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-05-13 23:59:39 -0500

    1

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -74,4 +74,32 @@ return {
             vim.keymap.del({ "x", "o" }, "X")
         end,
     },
+    {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup {}
+        end,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            {
+                "zbirenbaum/copilot-cmp",
+                config = function()
+                    require("copilot_cmp").setup()
+                end,
+            },
+        },
+        opts = {
+            sources = {
+                { name = "nvim_lsp", group_index = 2 },
+                { name = "copilot", group_index = 2 },
+                { name = "luasnip", group_index = 2 },
+                { name = "buffer", group_index = 2 },
+                { name = "nvim_lua", group_index = 2 },
+                { name = "path", group_index = 2 },
+            },
+        },
+    },
 }
